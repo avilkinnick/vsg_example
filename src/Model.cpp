e46fae892b9895bf825f1c41adf56ae7d026ea5a
avilkinnick <avilkin.nick@gmail.com>
2024-03-15 09:43:33 +0300

    Поменял обход массивов

--- src/Model.cpp
+++ src/Model.cpp
@@ -25,18 +25,12 @@ Model Model::loadDmd(const vsg::Path& path, vsg::ref_ptr<const vsg::Options> opt
         std::stringstream stream(stringValue->value());
 
         // Указатель на текущий меш
-        Mesh* mesh = nullptr;
+        Mesh* currentMesh = nullptr;
 
         // Временные массивы для хранения координат вершин и индексов модели
         // до их привязки к текстурным координатам и индексам
         vsg::ref_ptr<vsg::vec3Array> tempVertices;
         vsg::ref_ptr<vsg::ushortArray> tempIndices;
-
-        std::size_t verticesCount;
-        std::size_t facesCount;
-        std::size_t indicesCount;
-
-        std::size_t tempVerticesCount;
 
         std::string input;
         while (std::getline(stream, input))
@@ -45,62 +39,71 @@ Model Model::loadDmd(const vsg::Path& path, vsg::ref_ptr<const vsg::Options> opt
 
             if (input == "New object")
             {
-                mesh = &(model.meshes_.emplace_back(Mesh()));
+                currentMesh = &(model.meshes_.emplace_back(Mesh()));
                 tempVertices.reset();
                 tempIndices.reset();
-                verticesCount = 0;
-                indicesCount = 0;
-                tempVerticesCount = 0;
             }
             else if (input == "numverts numfaces")
             {
+                std::size_t tempVerticesCount;
+                std::size_t facesCount;
+
                 stream >> tempVerticesCount >> facesCount;
-                indicesCount = facesCount * 3;
+
+                tempVertices = vsg::vec3Array::create(tempVerticesCount);
+                tempIndices = vsg::ushortArray::create(facesCount * 3);
             }
             else if (input == "Mesh vertices:")
             {
-                tempVertices = vsg::vec3Array::create(tempVerticesCount);
-                for (std::size_t i = 0; i < tempVerticesCount; ++i)
+                for (auto& tempVertex : *tempVertices)
                 {
-                    stream >> tempVertices->at(i);
+                    stream >> tempVertex;
                 }
             }
             else if (input == "Mesh faces:")
             {
-                tempIndices = vsg::ushortArray::create(indicesCount);
-                for (std::size_t i = 0; i < indicesCount; ++i)
+                for (auto& tempIndex : *tempIndices)
                 {
-                    std::size_t index;
-                    stream >> index;
-                    tempIndices->at(i) = index - 1;
+                    stream >> tempIndex;
+                    --tempIndex;
                 }
             }
             else if (input == "numtverts numtvfaces")
             {
+                std::size_t verticesCount;
+                std::size_t facesCount;
+
                 stream >> verticesCount >> facesCount;
+
+                currentMesh->vertices = vsg::vec3Array::create(verticesCount);
+                currentMesh->normals = vsg::vec3Array::create(verticesCount);
+                currentMesh->texCoords = vsg::vec3Array::create(verticesCount);
+                currentMesh->colors = vsg::vec4Array::create(verticesCount);
+                currentMesh->indices = vsg::ushortArray::create(facesCount * 3);
             }
             else if (input == "Texture vertices:")
             {
-                mesh->texCoords = vsg::vec3Array::create(verticesCount);
-                mesh->colors = vsg::vec4Array::create(verticesCount);
-                for (std::size_t i = 0; i < verticesCount; ++i)
+                for (auto& texCoord : *currentMesh->texCoords)
                 {
-                    stream >> mesh->texCoords->at(i);
+                    stream >> texCoord;
+                }
 
-                    mesh->colors->at(i).set(1.0f, 1.0f, 1.0f, 1.0f);
+                for (auto& color : *currentMesh->colors)
+                {
+                    color.set(1.0f, 1.0f, 1.0f, 1.0f);
                 }
             }
             else if (input == "Texture faces:")
             {
-                mesh->indices = vsg::ushortArray::create(indicesCount);
-                mesh->vertices = vsg::vec3Array::create(verticesCount);
-
                 std::set<std::size_t> processedIndices;
+                std::size_t indicesCount = currentMesh->indices->size();
+
                 for (std::size_t i = 0; i < indicesCount; ++i)
                 {
                     std::size_t index;
                     stream >> index;
-                    mesh->indices->at(i) = --index;
+                    --index;
+                    currentMesh->indices->at(i) = index;
 
                     // Если вершина с заданным индексом еще не была обработана,
                     // привязать координаты этой вершины
@@ -108,33 +111,29 @@ Model Model::loadDmd(const vsg::Path& path, vsg::ref_ptr<const vsg::Options> opt
                     if (processedIndices.find(index) == processedIndices.end())
                     {
                         std::size_t tempIndex = tempIndices->at(i);
-                        mesh->vertices->at(index) = tempVertices->at(tempIndex);
+                        currentMesh->vertices->at(index) = tempVertices->at(tempIndex);
                         processedIndices.insert(index);
                     }
                 }
 
                 // Посчитать нормали вершин
-                mesh->normals = vsg::vec3Array::create(verticesCount);
+                std::size_t facesCount = currentMesh->indices->size() / 3;
                 for (std::size_t i = 0; i < facesCount; ++i)
                 {
-                    auto index1 = mesh->indices->at(i * 3);
-                    auto index2 = mesh->indices->at(i * 3 + 1);
-                    auto index3 = mesh->indices->at(i * 3 + 2);
+                    auto index1 = currentMesh->indices->at(i * 3);
+                    auto index2 = currentMesh->indices->at(i * 3 + 1);
+                    auto index3 = currentMesh->indices->at(i * 3 + 2);
 
-                    auto& v1 = mesh->vertices->at(index1);
-                    auto& v2 = mesh->vertices->at(index2);
-                    auto& v3 = mesh->vertices->at(index3);
+                    auto& v1 = currentMesh->vertices->at(index1);
+                    auto& v2 = currentMesh->vertices->at(index2);
+                    auto& v3 = currentMesh->vertices->at(index3);
 
-                    auto& n1 = mesh->normals->at(index1);
-                    auto& n2 = mesh->normals->at(index2);
-                    auto& n3 = mesh->normals->at(index3);
-
-                    // Векторы a и b - 2 стороны полигона
-                    vsg::vec3 a = v2 - v1;
-                    vsg::vec3 b = v3 - v1;
+                    auto& n1 = currentMesh->normals->at(index1);
+                    auto& n2 = currentMesh->normals->at(index2);
+                    auto& n3 = currentMesh->normals->at(index3);
 
                     // Вектор c - это нормальнь поверхности
-                    vsg::vec3 faceNormal = vsg::cross(a, b);
+                    vsg::vec3 faceNormal = vsg::cross(v2 - v1, v3 - v1);
 
                     // Добавить к каждой нормали вершины нормаль поверхности,
                     // на которой она лежит
@@ -148,5 +147,3 @@ Model Model::loadDmd(const vsg::Path& path, vsg::ref_ptr<const vsg::Options> opt
 
     return model;
 }
-
-
