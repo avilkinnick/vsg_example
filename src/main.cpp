d73863221a2f0f4a42e312f66d3de3d9962d0686
avilkinnick <avilkin.nick@gmail.com>
2024-03-22 02:46:36 +0300

    Получилось загрузить несколько моделей

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,17 @@ int main(int argc, char* argv[])
     vsg_Instance vsg_instance(&argc, argv);
 
     // Загрузить модель
-    auto model = DMD_Reader().read(vsg_instance.arguments[1],
+    auto model1 = DMD_Reader().read(vsg_instance.arguments[1],
                                    vsg_instance.arguments[2],
                                    vsg_instance.options);
 
-    vsg_instance.sceneGraph->addChild(model);
+    vsg_instance.sceneGraph->addChild(model1);
+
+    auto model2 = DMD_Reader().read(vsg_instance.arguments[3],
+                                    vsg_instance.arguments[4],
+                                    vsg_instance.options);
+
+    vsg_instance.sceneGraph->addChild(model2);
 
     // После передачи модели инициализировать все остальное и запустить приложение
     vsg_instance.run();
